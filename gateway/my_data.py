42ef12c60c652282091dcf92d0650d1f122e6083
Lam Phat <phat.vuhuynh@hcmut.edu.vn>
2024-05-26 22:27:53 +0700

    update remove_outlier()

--- gateway/my_data.py
+++ gateway/my_data.py
@@ -30,7 +30,7 @@ class Data:
 
     def get_all_data(self, feed_list, value_list):
         while len(feed_list) != 0:
-            print("in get all data")
+            # print("in get all data")
             self.get_single_data(feed_list[0], value_list[0])
             feed_list.pop(0)
             value_list.pop(0)
@@ -74,16 +74,22 @@ class Data:
 
         q1, _, q3 = statistics.quantiles(arr)
         iqr = q3 - q1
-
+        # print(arr)
         while std > sample_std and (arr[0] < q1 - iqr * 1.5 or arr[-1] > q3 + iqr * 1.5):
             if ((q1 - iqr * 1.5) - arr[0]) < (arr[-1] - (q3 + iqr * 1.5)):
                 arr.pop(-1)
             else:
                 arr.pop(0)
-
+            
+            # print(q1)
+            # print(q3)
+            # print(q1 - iqr * 1.5)
+            # print(q3 + iqr * 1.5)
+            # print(std)
+            # print(arr)
             std = statistics.pstdev(arr)
 
-            q1, _, q3 = statistics.quantiles(self.brightness)
+            q1, _, q3 = statistics.quantiles(arr)
             iqr = q3 - q1
 
         match cmd:
@@ -125,93 +131,6 @@ class Data:
             self.remove_outlier('humid')
             self.remove_outlier('soil')
             self.remove_outlier('temp')
-            # self.mean_temp = statistics.mean(self.temperature)
-            # self.mean_bright = statistics.mean(self.brightness)
-            # self.mean_soil = statistics.mean(self.soil_moisture)
-            # self.mean_humid = statistics.mean(self.humidity)
-
-            # self.std_bright = statistics.pstdev(self.brightness)
-            # self.std_humid = statistics.pstdev(self.humidity)
-            # self.std_soil = statistics.pstdev(self.soil_moisture)
-            # self.std_temp = statistics.pstdev(self.temperature)
-
-            # self.temperature.sort()
-            # self.brightness.sort()
-            # self.soil_moisture.sort()
-            # self.humidity.sort()
-
-            # q1, _, q3 = statistics.quantiles(self.brightness)
-            # iqr = q3 - q1
-
-            # while self.std_bright > 5 and (self.brightness[0] < q1 - iqr * 1.5 or self.brightness[-1] > q3 + iqr * 1.5):
-            #     if ((q1 - iqr * 1.5) - self.brightness[0]) < (self.brightness[-1] - (q3 + iqr * 1.5)):
-            #         self.brightness.pop(-1)
-            #     else:
-            #         self.brightness.pop(0)
-
-            #     self.std_bright = statistics.pstdev(self.brightness)
-            #     # print(self.std_bright)
-
-            #     q1, _, q3 = statistics.quantiles(self.brightness)
-            #     iqr = q3 - q1
-
-            # self.mean_bright = round(statistics.mean(self.brightness), 2)
-
-            # q1, _, q3 = statistics.quantiles(self.humidity)
-            # iqr = q3 - q1
-
-            # while self.std_humid > 10 and (self.humidity[0] < q1 - iqr * 1.5 or self.humidity[-1] > q3 + iqr * 1.5):
-            #     if ((q1 - iqr * 1.5) - self.humidity[0]) < (self.humidity[-1] - (q3 + iqr * 1.5)):
-            #         self.humidity.pop(-1)
-            #     else:
-            #         self.humidity.pop(0)
-            #     self.std_humid = statistics.pstdev(self.humidity)
-            #     # print(self.std_humid)
-            #     q1, _, q3 = statistics.quantiles(self.humidity)
-            #     iqr = q3 - q1
-
-            # self.mean_humid = round(statistics.mean(self.humidity), 2)
-
-            # q1, _, q3 = statistics.quantiles(self.soil_moisture)
-            # iqr = q3 - q1
-
-            # while self.std_soil > 0.2 and (self.soil_moisture[0] < q1 - iqr * 1.5 or self.soil_moisture[-1] > q3 + iqr * 1.5):
-            #     if ((q1 - iqr * 1.5) - self.soil_moisture[0]) < (self.soil_moisture[-1] - (q3 + iqr * 1.5)):
-            #         self.soil_moisture.pop(-1)
-            #     else:
-            #         self.soil_moisture.pop(0)
-            #     self.std_soil = statistics.pstdev(self.soil_moisture)
-            #     # print(self.std_soil)
-            #     q1, _, q3 = statistics.quantiles(self.soil_moisture)
-            #     iqr = q3 - q1
-
-            # self.mean_soil = round(statistics.mean(self.soil_moisture), 2)
-
-            # q1, _, q3 = statistics.quantiles(self.temperature)
-            # iqr = q3 - q1
-            # # print(q1)
-            # # print(q3)
-            # # print(q1 - iqr * 1.5)
-            # # print(q3 + iqr * 1.5)
-            # # print(self.std_temp)
-
-            # while self.std_temp > 0.8 and (self.temperature[0] < q1 - iqr * 1.5 or self.temperature[-1] > q3 + iqr * 1.5):
-            #     print(self.temperature)
-            #     if ((q1 - iqr * 1.5) - self.temperature[0]) < (self.temperature[-1] - (q3 + iqr * 1.5)):
-            #         self.temperature.pop(-1)
-            #     else:
-            #         self.temperature.pop(0)
-            #     # print(q1)
-            #     # print(q3)
-            #     # print(q1 - iqr * 1.5)
-            #     # print(q3 + iqr * 1.5)
-            #     # print(self.std_temp)
-            #     self.std_temp = statistics.pstdev(self.temperature)
-
-            #     q1, _, q3 = statistics.quantiles(self.temperature)
-            #     iqr = q3 - q1
-
-            # self.mean_temp = statistics.mean(self.temperature)
 
             self.temperature.clear()
             self.humidity.clear()
@@ -232,7 +151,7 @@ my_data = Data()
 
 # CMD = ["TEMP", "HUMID", "SOIL", "BRIGHT", "LED", "FAN", "PUMP", "control"]
 # for test
-# feed_list = ["TEMP", "TEMP", "TEMP","HUMID", "HUMID", "HUMID","TEMP", "HUMID", "TEMP", "HUMID", "SOIL", "BRIGHT", "TEMP", "TEMP","HUMID", "HUMID", "HUMID","TEMP", "HUMID", "TEMP", "HUMID", "SOIL", "BRIGHT"]
-# value_list = [0,     29.5,     42,     82,      90,     91,    28,      66,     28,      88,     70,     65,        26,   29.5,     42,     82,        90,    91,    30,     150,       35,     88,        70]
-# my_data.get_all_data(feed_list, value_list)
-# my_data.calculate()
+feed_list = ["TEMP", "TEMP", "TEMP","HUMID", "HUMID", "HUMID","TEMP", "HUMID", "TEMP", "HUMID", "SOIL", "BRIGHT", "TEMP", "TEMP","HUMID", "HUMID", "HUMID","TEMP", "HUMID", "TEMP", "HUMID", "SOIL", "BRIGHT"]
+value_list = [0,     29.5,     42,     82,      90,     91,    28,      66,     28,      88,     70,     65,        26,   29.5,     42,     82,        90,    91,    30,     150,       35,     88,        70]
+my_data.get_all_data(feed_list, value_list)
+my_data.calculate()
